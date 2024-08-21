4b050a3f59be4477cb0f71ec9b4dce13d0411665
Lucas Dantas <lucasdantas.dev@gmail.com>
2024-08-21 09:09:18 -0300

    Removes global variable usage.

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ text_to_speech = TTS("tts_models/multilingual/multi-dataset/xtts_v2")
 messages = []
 output_dir = "audio_outputs/"
 input_dir = "audio_inputs/"
-last_audio_sample = input_dir + "female.wav"
 
 # Basic Flask route to handle the chat page (the only page in this example)
 @app.route('/', methods=['GET', 'POST'])
@@ -25,7 +24,8 @@ def chat():
             try:
                 audio_filename = generate_random_filename()
                 output_path = output_dir + audio_filename
-                text_to_speech.tts_to_file(text=user_message, file_path=output_path, speaker_wav=last_audio_sample, language="en")
+                speaker_wav = get_latest_audio_sample()
+                text_to_speech.tts_to_file(text=user_message, file_path=output_path, speaker_wav=speaker_wav, language="en")
                 messages.append({'type': 'audio', 'content': audio_filename})
                 return jsonify({'status': 'success', 'audio_url': url_for('get_audio', filename=audio_filename)})
             except Exception as e:
@@ -39,12 +39,9 @@ def upload_voice_sample():
         audio_file = request.files['audio']
         audio_filename = generate_random_filename()
         input_path = input_dir + audio_filename
-        last_audio_sample = input_path
         audio_file.save(input_path)
-
         messages.append({'type': 'audio', 'content': audio_filename})
         return jsonify({'status': 'success', 'audio_url': url_for('get_audio', filename=audio_filename)})
-    
     return jsonify({'status': 'error', 'message': 'No audio file uploaded.'})
 
 @app.route('/get_audio/<filename>', methods=['GET'])
@@ -54,6 +51,10 @@ def get_audio(filename):
     elif os.path.exists(input_dir + filename):
         return send_file(input_dir + filename, mimetype='audio/wav')
     return jsonify({'status': 'error', 'message': 'No audio file found.'})
+
+def get_latest_audio_sample():
+    files = [os.path.join(input_dir, file) for file in os.listdir(input_dir) if os.path.isfile(os.path.join(input_dir, file))]
+    return max(files, key=os.path.getctime)
 
 def generate_random_filename():
     return f"audio_{random.randint(1000, 9999)}.wav"
